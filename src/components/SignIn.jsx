d881033fc902df95489e8f8a67b4250059b45a06
unknown <robin@digitalguru.buzz>
2021-03-20 18:44:01 +0530

    update

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -4,7 +4,7 @@ export default function SignIn() {
   return (
     <>
       <p>
-        how many times have you thought of giving back to society? As per a survey, an individual contemplates charity several times a year but for most, it just ends up as a thought in the head. Why is that? Why don't we give back to society when we want to? As per another survey, people have a notion that the process of joining a charitable organization is tedious. There is a lot of paperwork that they don't want to do.
+        How many times have you thought of giving back to society? As per a survey, an individual contemplates charity several times a year but for most, it just ends up as a thought in the head. Why is that? Why don't we give back to society when we want to? As per another survey, people have a notion that the process of joining a charitable organization is tedious. There is a lot of paperwork that they don't want to do.
       </p>
       <p>
         To tackle this, we have built a blockchain-based platform to connect NGOs and charitable organizations with volunteers.
